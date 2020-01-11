a919b24837c6314b4b9c7a2cc6a4c3554f442c44
yuri-hu <YuriHuGL@outlook.com>
2020-01-11 19:41:36 +0800

    File copy
    Resolve #1
    
    Signed-off-by: yuri-hu <YuriHuGL@outlook.com>

---
+++ file-copy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define SIZE 1024
+
+int main(int args, char *argv[])
+{
+	char buf[SIZE];
+	int fd, f;
+    const char *fin = "a.txt";
+    const char *fout = "b.txt";
+	fd = open(fin, O_RDONLY);
+
+	if (fd < 0) {
+		perror(fin);
+
+		exit(1);
+	}
+
+	f = open(fout, O_CREAT | O_WRONLY, S_IRWXU);
+
+	while (read(fd, buf, 1) > 0)
+		write(f, buf, 1);
+
+	close(fd);
+	close(f);
+
+	return 0;
+}
