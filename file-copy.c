f0f82f082c8ac9a26e35c564504b22929906fa71
Yuri Hu <53166777+yuri-hu@users.noreply.github.com>
2020-01-11 21:57:45 +0800

    Update file-copy.c

--- file-copy.c
+++ file-copy.c
@@ -8,18 +8,21 @@ int main(int args, char *argv[])
 {
 	char buf[SIZE];
 	int fd, f;
-    const char *fin = "a.txt";
-    const char *fout = "b.txt";
+	const char *fin = "a.txt";
+	const char *fout = "b.txt";
+	
 	fd = open(fin, O_RDONLY);
-
 	if (fd < 0) {
 		perror(fin);
-
 		exit(1);
 	}
 
 	f = open(fout, O_CREAT | O_WRONLY, S_IRWXU);
-
+	if (f < 0) {
+		perror(fout);
+		exit(1);
+	}
+	
 	while (read(fd, buf, 1) > 0)
 		write(f, buf, 1);
 
